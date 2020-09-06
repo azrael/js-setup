517dfe0d1e65a522c3bcd333c8d4dad249fd5257
azrael <azrael.25.91@gmail.com>
2020-09-06 23:36:29 +0300

    first commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+const autoprefixer = require('autoprefixer'),
+    presetEnv = require('postcss-preset-env'),
+    cssnano = require('cssnano'),
+    normalize = require('postcss-normalize');
+
+module.exports = {
+    plugins: [normalize, presetEnv, autoprefixer, cssnano]
+};
